d8aac62f7153391a3f2ef9c2353d0d80a6aae7ea
Everton Santos de Castro <evertonsantosdecastro@gmail.com>
2025-07-20 20:03:51 -0300

    First tip - Transform the value in local currency

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Hello, World!\n");
+
+
+decimal valor = 1580.20M;
+
+// Currency based on your location. Add ":C" after the value you want to convert to your local currency.
+Console.WriteLine($"{valor:C}");
