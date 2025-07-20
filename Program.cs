d7e6fd42659016db69cc82cc84b0354a5d447dbe
Everton Santos de Castro <evertonsantosdecastro@gmail.com>
2025-07-20 20:38:22 -0300

    Adding tip 2 and removing bin and obj folders

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Hello, World!\n");
+﻿using System.Globalization;
+
+Console.WriteLine("Hello, World!\n");
 
 
 //#region Tip 1
@@ -7,3 +9,12 @@ decimal valor = 1580.20M;
 Console.WriteLine($"{valor:C}");
 
 //#endregion Tip 1
+
+//#region Tip 2
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");     // United States
+Console.WriteLine($"{valor:C}");
+
+//CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");     // Brazil
+
+//#endregion Tip 2
